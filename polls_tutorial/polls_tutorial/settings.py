7c33aa096a97f8b86f9ff7fb9cd7a21411c0c4ac
Your Name <you@example.com>
2024-09-04 15:20:30 +0800

    add password reset

--- polls_tutorial/polls_tutorial/settings.py
+++ polls_tutorial/polls_tutorial/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +139,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER_NAME')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_APP_PASSWORD')
